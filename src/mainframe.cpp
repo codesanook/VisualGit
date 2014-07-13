b1c954cd1a199c35a8c0bfe4769f1c4504becec2
Putta Khunchalee <ultimaweapon@outlook.com>
2014-07-13 18:09:01 +0700

    Add application icons.

--- src/mainframe.cpp
+++ src/mainframe.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "mainframe.h"
+#include "app.h"
 
 IMPLEMENT_DYNAMIC_CLASS(MainFrame, wxFrame)
 
@@ -13,6 +14,12 @@ MainFrame::MainFrame()
 MainFrame::MainFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
         : wxFrame((wxFrame *) NULL, wxID_ANY, title, pos, size)
 {
+    // set icons
+    wxIconBundle icons;
+    icons.AddIcon(App::GetIconFileName(wxT("visualgit.png"), 16, 16), wxBITMAP_TYPE_PNG);
+    icons.AddIcon(App::GetIconFileName(wxT("visualgit.png"), 32, 32), wxBITMAP_TYPE_PNG);
+    icons.AddIcon(App::GetIconFileName(wxT("visualgit.png"), 48, 48), wxBITMAP_TYPE_PNG);
+    SetIcons(icons);
 }
 
 MainFrame::~MainFrame()
